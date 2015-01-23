b4fa6fd6312f70350df4cfe393eb5bb2a39fd5e7
unknown <i.dimcevski@gmail.com>
2015-01-23 21:39:32 +0100

    camera aspekt ratio + movement

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+            transform.Translate(new Vector3(-11, 0, 0));
+        if (Input.GetButtonDown("Fire2"))
+            transform.Translate(new Vector3(11, 0, 0));
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Translate(new Vector3(-1, 0, 0));
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(new Vector3(1, 0, 0));
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //    transform.position = Vector3.Lerp(transform.position, transform.position + new Vector3(10, 0, 0), 0.15f);
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //    transform.Translate(new Vector3(1, 0, 0));
+    }
+}
