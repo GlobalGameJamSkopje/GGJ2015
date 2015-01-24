435552ce59f4bc0aa3f4f5f33be748da2962ae3c
unknown <i.dimcevski@gmail.com>
2015-01-24 15:31:24 +0100

    Code timer

--- Assets/LevelLogic.cs
+++ Assets/LevelLogic.cs
@@ -1,21 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LevelLogic : MonoBehaviour {
-    
+public class LevelLogic : MonoBehaviour
+{
+
     public GameObject characterPast;
     public GameObject characterPresent;
     public GameObject characterFuture;
 
-	void Start () {
-	
-	}
-	
-	void Update () {
-	
-	}
+    private float timeLeft;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (GeneralData.InGame)
+        {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                GameOver();
+            }
+        }
+        
+    }
+    void GameOver()
+    {
+        Debug.Log("GAMEOVER");
+    }
+
     void ProceedNextLevel()
     {
-        transform.SendMessage("CameraMoveRight");        
+        transform.SendMessage("CameraMoveRight");
     }
+    void ResetTimer()
+    {
+        timeLeft = 60;
+    }
+
 }
