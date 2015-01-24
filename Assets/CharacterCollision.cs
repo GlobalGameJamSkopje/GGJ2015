708096884b54d03a67ef4305c395486e50eedc74
unknown <i.dimcevski@gmail.com>
2015-01-24 04:15:49 +0100

    movement characters

---
+++ Assets/CharacterCollision.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterCollision : MonoBehaviour
+{
+    public GameObject tCamera;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        var collider = collision.collider;
+
+        Vector3 contactPoint = collision.contacts[0].point;
+        Vector3 center = collider.bounds.center;
+
+        bool left = collision.contacts[0].normal == Vector3.right;//contactPoint.x > center.x;
+        bool right = collision.contacts[0].normal == Vector3.left;//contactPoint.x < center.x;
+        bool bot = collision.contacts[0].normal == Vector3.up;//contactPoint.y > center.y;
+        bool top = collision.contacts[0].normal == Vector3.down;//contactPoint.y < center.y;
+        Debug.Log(" left: " + left + " right: " + right + " bot: " + bot + " top: " + top);
+        
+        if (left)
+        {
+            Debug.Log("The object is to the left");
+            tCamera.SendMessage("CollisionLeft");
+        }
+        if (top)
+        {
+            Debug.Log("The object is to the top");
+            tCamera.SendMessage("CollisionTop");
+        }
+        if (bot)
+        {
+            Debug.Log("The object is to the bot");
+            tCamera.SendMessage("CollisionBot");
+        }
+        if (right)
+        {
+            Debug.Log("The object is to the right");
+            tCamera.SendMessage("CollisionRight");            
+        }
+    }
+}
---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+    public GameObject characterPast;
+    public GameObject characterPresent;
+    public GameObject characterFuture;
+
+    private float moveSpeed = 3f;
+    private bool canMoveLeft = true;
+    private bool canMoveTop = true;
+    private bool canMoveRight = true;
+    private bool canMoveBot = true;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (false) // ova ako testiras da ti se dvizat vo site nasoki idi so true
+        {
+            characterPast.transform.Translate(Vector3.up * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+            characterFuture.transform.Translate(Vector3.up * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+            characterPresent.transform.Translate(Vector3.up * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+            characterPast.transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+            characterPresent.transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+            characterFuture.transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+        }
+        else
+        {
+            if (Input.GetAxis("Vertical") > 0) //ide nagore
+            {
+                if (canMoveTop)
+                {
+                    characterPast.transform.Translate(Vector3.up * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+                    characterFuture.transform.Translate(Vector3.up * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+                    characterPresent.transform.Translate(Vector3.up * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+                }
+            }
+            else // ide nadole
+            {
+                if (canMoveBot)
+                {
+                    characterPast.transform.Translate(Vector3.up * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+                    characterFuture.transform.Translate(Vector3.up * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+                    characterPresent.transform.Translate(Vector3.up * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+                }
+            }
+            if (Input.GetAxis("Horizontal") > 0) //ide desno
+            {
+                if (canMoveRight)
+                {
+                    characterPast.transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+                    characterPresent.transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+                    characterFuture.transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+                }
+            }
+            else // ide levo
+            {
+                if (canMoveLeft)
+                {
+                    characterPast.transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+                    characterPresent.transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+                    characterFuture.transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+                }
+            }
+        }
+        //canMoveLeft = true;
+        //canMoveTop = true;
+        //canMoveRight = true;
+        //canMoveBot = true;
+
+    }
+    public void CollisionLeft()
+    {
+        canMoveLeft = false;
+    }
+    public void CollisionTop()
+    {
+        canMoveTop = false;
+    }
+    public void CollisionRight()
+    {
+        canMoveRight = false;
+    }
+    public void CollisionBot()
+    {
+        canMoveBot = false;
+    }
+}
