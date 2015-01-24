d578a6abedc2710f22bbce6ff71d9a433d4acd31
Oliver Lazarevski <oliver.lazarevski@gmail.com>
2015-01-24 01:33:27 +0100

    Onaka movement

---
+++ Assets/MovementScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour {
+
+    private float moveSpeed = 3f;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+            //Moves Forward and back along z axis                           //Up/Down
+            transform.Translate(Vector3.up * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+            //Moves Left and right along x Axis                               //Left/Right
+            transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+        
+        
+
+	}
+
+    
+}
