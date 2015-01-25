c7e8570b1487bd2de29e732a8fd8cc3b56b773e7
Oliver Lazarevski <oliver.lazarevski@gmail.com>
2015-01-25 01:08:41 +0100

    Movable Objects Level-3-4s

---
+++ Assets/MovableScriptLevel3.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovableScriptLevel3 : MonoBehaviour
+{
+
+    public GameObject CharacterPast;
+    private bool isMoved = false;
+    void Start () {
+	}
+	
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!isMoved)
+        {
+            if (collision.gameObject == CharacterPast)
+            {
+                gameObject.transform.Translate(-0.7f, 0, 0);
+                isMoved = true;
+            }            
+        }
+    }
+}
---
+++ Assets/MovableScriptLevel4.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovableScriptLevel4 : MonoBehaviour
+{
+    public GameObject CharacterPast;
+    public GameObject CharacterPresent;
+    private int pastMoved;
+    private int presentMoved;
+
+    void Start()
+    {
+        presentMoved = 0;
+        pastMoved = 0;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == CharacterPast)
+        {
+            if (pastMoved < 1)
+            {
+                gameObject.transform.Translate(+0.7f, 0, 0);
+                pastMoved++;
+            }
+        }
+        if (collision.gameObject == CharacterPresent)
+        {
+            if (presentMoved < 3)
+            {
+                gameObject.transform.Translate(-0.7f, 0, 0);
+                presentMoved++;
+            }
+        }        
+    }
+}
