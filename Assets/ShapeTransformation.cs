e2c4f478434ef8f367e9d5f95b9f552d73798ced
Oliver Lazarevski <oliver.lazarevski@gmail.com>
2015-01-25 02:30:18 +0100

    ShapeShifting

---
+++ Assets/ShapeTransformation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShapeTransformation : MonoBehaviour {
+    public GameObject CharacterPresent;
+    public ColliderTag ToShape;
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("ShapeTransformation");
+        CharacterPresent.tag = ToShape.ToString();
+        Destroy(gameObject);
+    }
+}
